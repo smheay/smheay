cc6e4dffe4c062fc6fc2b9dfe0251beb8fa03628
Shannon <shmeyer@pdx.edu>
2020-04-25 15:32:36 -0700

    link fix
    link location

--- JS/script.js
+++ JS/script.js
@@ -6,4 +6,17 @@ $(document).ready(function () {
   //Loads the Nav bar
   $("#nav-placeholder").load("/Content/navbar.html");
 
+
+  //Sets the active class on nav bar
+  $(function(){
+    var current = location.pathname;
+    $('#navList li a').each(function(){
+        var $this = $(this);
+        // if the current path is like this link, make it active
+        if($this.attr('href').indexOf(current) !== -1){
+            $this.addClass('active');
+        }
+    })
+})
+
 });
