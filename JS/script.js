5f24d0841312344df7afa75f127f0a79758d673e
Shannon <shmeyer@pdx.edu>
2020-04-30 10:29:00 -0700

    skeleton updates to fill text
    Contact, homepage, nav and projects updated with frames to fill

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,14 @@
-//https://www.digitalocean.com/community/tutorials/an-introduction-to-jquery
+// https://www.digitalocean.com/community/tutorials/an-introduction-to-jquery
 
 $(document).ready(function () {
-  // all custom jQuery will go here
+    // all custom jQuery will go here
 
-  //Loads the Nav bar
-  $("#nav-placeholder").load("/Content/navbar.html");
+    // Loads the Nav bar
+    $("#nav-placeholder").load("/Content/navbar.html");
 
 
-  //Sets the active class on nav bar
-  $(function(){
-    var current = location.pathname;
-    $('#navList li a').each(function(){
-        var $this = $(this);
-        // if the current path is like this link, make it active
-        if($this.attr('href').indexOf(current) !== -1){
-            $this.addClass('active');
-        }
-    })
-})
+    // Sets the active class on nav bar
+ 
+
 
 });
